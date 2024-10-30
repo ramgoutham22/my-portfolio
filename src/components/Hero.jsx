031c7eb2c96d4b27db29b06715d5e8588ba5d4ac
ramgoutham22 <103619596+ramgoutham22@users.noreply.github.com>
2024-10-30 15:43:36 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,11 +8,13 @@ const Hero = () => {
       <div className='w-full md:w-1/2 mb-10 text-center md:text-left'>
         <h3 className='text-base md:text-lg lg:text-xl mb-0 md:-mb-2 text-yellow-500'>Hello, I am </h3>
         <h1 className='text-2xl md:text-4xl lg:text-6xl my-0 md:-ml-1 font bold py-2 md:py:2'>Ram Goutham</h1>
-        <h3 className='text-base md:text-xl lg:text-2xl text-green-600'>Product Enginner & Web developer</h3>
+        <h3 className='text-base md:text-xl lg:text-2xl text-green-600'>Product Engineer & Web developer</h3>
         <p className='text-sm md:text-xl lg:text-lg tracking-tighter'>Experienced Product Engineer working at Tata Consultancy Services having industrial hands on work experience with Java, JavaScript and also has a college level experience in Python. Able to debug and fix the issue. Uses Eclipse as an IDE. Currently working on the project "Transform Plus Reverse Engieering".
         </p>
         <div className='flex items-center justify-content md:justify-start gap-4 mt-5'> 
+          <a href="https://api.whatsapp.com/send?phone=919848759689"> 
             <button className='bg-yellow-500 px-10 py-1 rounded-large mr-4 hover:bg-yellow-400 duration-200'>Hire Me</button>
+            </a>
             <a className='flex items-center text-yellow-500 font-semibold' href={Goutham_CV} download="Goutham_CV.pdf"> 
                 Download CV <BsDownload className='ml-2 text-lg'/> 
 
